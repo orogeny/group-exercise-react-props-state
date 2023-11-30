67c759c982ccc77f3af393e4805c367f78c9cde0
Paul <paulwharin@gmail.com>
2023-11-30 13:09:35 +0000

    feat: implement missing functionality in chuck_card/info and joke components

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,7 @@
 interface ChuckJokeProps {
-
+  joke: string;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
-
-
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ joke }) => <p>{joke}</p>;
 
 export default ChuckJoke;
